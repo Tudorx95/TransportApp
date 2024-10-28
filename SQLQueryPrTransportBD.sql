41c32b7814ba7faf3d710ca0eaecd455ad473490
george.stanciu <george.stanciu@mta.ro>
2024-10-28 19:18:32 +0200

    New query pt DB - with tables populated

--- SQLQueryPrTransportBD.sql
+++ SQLQueryPrTransportBD.sql
@@ -1,4 +1,16 @@
-﻿-- Creare baza de date
+﻿--drop tables if they exist------------------------------------------------------
+DROP TABLE IF EXISTS Statie;
+DROP TABLE IF EXISTS MT;
+DROP TABLE IF EXISTS Traseu;
+DROP TABLE IF EXISTS TipMT;
+DROP TABLE IF EXISTS Angajat;
+DROP TABLE IF EXISTS Tip_Angajat;
+DROP TABLE IF EXISTS Bilet;
+DROP TABLE IF EXISTS Tip_Bilet;
+DROP TABLE IF EXISTS [User];
+DROP TABLE IF EXISTS Persoana;
+DROP TABLE IF EXISTS Tip_User;
+-- Creare baza de date-----------------------------------------------------------
 CREATE DATABASE TransportDB;
 GO
 
@@ -7,14 +19,14 @@ GO
 
 -- Creare tabel Tip_User
 CREATE TABLE Tip_User (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     nume NVARCHAR(100) NOT NULL
 );
 GO
 
 -- Creare tabel Persoana
 CREATE TABLE Persoana (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     tip_persoana INT NOT NULL,
     nume NVARCHAR(100) NOT NULL,
     prenume NVARCHAR(100) NOT NULL,
@@ -26,7 +38,7 @@ GO
 
 -- Creare tabel User
 CREATE TABLE [User] (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     username NVARCHAR(50) NOT NULL,
     [password] NVARCHAR(50) NOT NULL,
     id_persoana INT NOT NULL,
@@ -36,14 +48,14 @@ GO
 
 -- Creare tabel Tip_Bilet
 CREATE TABLE Tip_Bilet (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     nume NVARCHAR(100) NOT NULL
 );
 GO
 
 -- Creare tabel Bilet
 CREATE TABLE Bilet (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     tip_bilet INT NOT NULL,
     valabilitate DATE NOT NULL,
     id_calator INT NOT NULL,
@@ -54,14 +66,14 @@ GO
 
 -- Creare tabel Tip_Angajat
 CREATE TABLE Tip_Angajat (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     nume NVARCHAR(100) NOT NULL
 );
 GO
 
 -- Creare tabel Angajat
 CREATE TABLE Angajat (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     salariu DECIMAL(10,2) NOT NULL,
     id_persoana INT NOT NULL,
     tip_angajat INT NOT NULL,
@@ -73,14 +85,14 @@ GO
 
 -- Creare tabel TipMT
 CREATE TABLE TipMT (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     nume NVARCHAR(100) NOT NULL
 );
 GO
 
 -- Creare tabel Traseu
 CREATE TABLE Traseu (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     statie_plecare NVARCHAR(100) NOT NULL,
     statie_sosire NVARCHAR(100) NOT NULL
 );
@@ -88,7 +100,7 @@ GO
 
 -- Creare tabel MT
 CREATE TABLE MT (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     id_tip INT NOT NULL,
     nr_traseu INT NOT NULL,
     nr_inmatriculare NVARCHAR(20) NOT NULL,
@@ -101,7 +113,7 @@ GO
 
 -- Creare tabel Statie
 CREATE TABLE Statie (
-    id_unic INT PRIMARY KEY IDENTITY(1,1),
+    id_unic INT PRIMARY KEY,
     id_traseu INT NOT NULL,
     denumire NVARCHAR(100) NOT NULL,
     timp_S_D TIME NOT NULL,
@@ -109,4 +121,68 @@ CREATE TABLE Statie (
     CONSTRAINT FK_Traseu_Statie FOREIGN KEY (id_traseu) REFERENCES Traseu(id_unic)
 );
 GO
+--pupulari tabele---------------------------------------------------------------------
+USE TransportDB;
+GO
 
+-- Populate Tip_User table with specified primary keys
+INSERT INTO Tip_User (id_unic, nume) VALUES 
+(1, 'Calator'), 
+(2, 'Angajat');
+GO
+
+-- Populate Persoana table with specified primary keys
+INSERT INTO Persoana (id_unic, tip_persoana, nume, prenume, CNP, adresa) VALUES 
+(1, 1, 'Popescu', 'Ion', '1234567890123', 'Str. Libertatii, Nr. 12'),
+(2, 2, 'Ionescu', 'Maria', '2345678901234', 'Str. Independentei, Nr. 8');
+GO
+
+-- Populate Tip_Bilet table with specified primary keys
+INSERT INTO Tip_Bilet (id_unic, nume) VALUES 
+(1, 'Abonament lunar'), 
+(2, 'Bilet o calatorie'),
+(3, 'Bilet dus-intors'),
+(4, 'Abonament 6 luni'),
+(5, 'Abonament 1 an');
+GO
+
+-- Populate Tip_Angajat table with specified primary keys
+INSERT INTO Tip_Angajat (id_unic, nume) VALUES 
+(1, 'Sofer'), 
+(2, 'Controlor');
+GO
+
+-- Populate Angajat table with specified primary keys
+INSERT INTO Angajat (id_unic, salariu, id_persoana, tip_angajat, data_angajarii) VALUES 
+(1, 3500.00, 2, 1, '2023-01-01'),
+(2, 2800.00, 2, 2, '2023-05-10');
+GO
+
+-- Populate TipMT table with specified primary keys
+INSERT INTO TipMT (id_unic, nume) VALUES 
+(1, 'Autobuz'), 
+(2, 'Tramvai'), 
+(3, 'Troleibuz'),
+(4, 'Metrou');
+GO
+
+-- Populate Traseu table with specified primary keys
+INSERT INTO Traseu (id_unic, statie_plecare, statie_sosire) VALUES 
+(1, 'Gara', 'Centru'), 
+(2, 'Centru', 'Aeroport'), 
+(3, 'Gara', 'Universitate');
+GO
+
+-- Populate MT table with specified primary keys
+INSERT INTO MT (id_unic, id_tip, nr_traseu, nr_inmatriculare, id_sofer) VALUES 
+(1, 1, 1, 'B123ABC', 1), 
+(2, 2, 2, 'B456DEF', 1);
+GO
+
+-- Populate Statie table with specified primary keys
+INSERT INTO Statie (id_unic, id_traseu, denumire, timp_S_D, timp_D_S) VALUES 
+(1, 1, 'Gara', '08:00', '18:00'),
+(2, 1, 'Centru', '08:30', '18:30'),
+(3, 2, 'Centru', '09:00', '19:00'),
+(4, 2, 'Aeroport', '09:45', '19:45');
+GO
