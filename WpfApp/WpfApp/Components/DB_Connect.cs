f314a706c9afa072c93b1be1f3c8dfa6aea82be1
Tudorx95 <tudorlepadatu@yahoo.com>
2024-11-02 00:17:53 +0200

    update

---
+++ WpfApp/WpfApp/Components/DB_Connect.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp.Components
+{
+    internal class DB_Connect
+    {
+        static public SqlConnection connect { get; private set; }
+        public DB_Connect(string ip,string db_name)
+        {
+            connect = connect_to_DB(ip, db_name);
+        }
+
+        static public void connection(string ip,string db_name)
+        {
+            connect = connect_to_DB(ip, db_name);
+        }
+        static private SqlConnection connect_to_DB(string ip, string db_name)
+        {
+            var connection = new SqlConnection();
+            connection.ConnectionString =
+            $"Server={ip};Database={db_name};Trusted_Connection=true";
+            connection.Open();
+            return connection;
+        }
+        static public DataTable GetTableByName(string tableName)
+        {
+            if (connect == null)
+                throw new InvalidOperationException("Database connection is not initialized.");
+
+            // Create a new SqlCommand and SqlDataAdapter to retrieve the table data
+            var query = $"SELECT * FROM [{tableName}]";
+            var cmd = new SqlCommand(query, connect);
+            var adapter = new SqlDataAdapter(cmd);
+            var dataTable = new DataTable(tableName);
+
+            // Fill the DataTable with the result
+            adapter.Fill(dataTable);
+
+            return dataTable;
+        }
+
+        static public bool IsDBConnected()
+        {
+            if(connect == null) return false;
+            return true;
+        }
+
+
+        static public void DB_Deconnect()
+        {
+            connect.Close();
+        }
+    }
+}
---
+++ WpfApp/WpfApp/Components/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp.Components
+{
+    class User
+    {
+        static public bool AddUser(string username, string password, int id_pers)
+        {
+            if (!DB_Connect.IsDBConnected())
+                return false;
+
+             string query = "INSERT INTO Users (username, password, id_persoana) VALUES (@username, @password, @id_pers)";
+
+            SqlCommand command = new SqlCommand(query, DB_Connect.connect);
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@password", password);
+            command.Parameters.AddWithValue("@id", id_pers);
+
+            try
+            {
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                // Handle exception (logging, rethrowing, etc.)
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+        static public bool Exist_User(string username,string password)
+        {
+            if (!DB_Connect.IsDBConnected())
+                return false;
+
+            string query = "select username,password from [User] where username=@username";
+            try
+            {
+                SqlCommand command = new SqlCommand(query, DB_Connect.connect);
+                command.Parameters.AddWithValue("@username", username);
+
+                SqlDataReader reader = command.ExecuteReader();
+                bool[] res = {false, false};
+
+                while (reader.Read())
+                {
+                    string user = reader["username"].ToString();                    
+                    string pass = reader["password"].ToString();
+
+                    if(user==username)
+                        res[0]=true;
+                    if(pass==password)
+                        res[1]=true;
+                }
+                reader.Close(); // Ensure the reader is closed
+
+                if (res[0] && res[1])
+                    return true;
+                else if (res[0])
+                    MessageBox.Show("Incorrect Password!");
+                else
+                    MessageBox.Show("Incorrect Username!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return false;
+        }
+    }
+}
